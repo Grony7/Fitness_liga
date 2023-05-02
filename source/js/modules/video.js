8e36a1df026d7e53380b63b9ffa0d60aabd73096
Grony <gr0ny.0017@gmail.com>
2023-05-02 16:48:38 +0300

    Финалочка

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -4,10 +4,22 @@ const video = document.querySelector('[data-video="video"]');
 
 export const playVideo = () => {
   if (playButton && video) {
-    playButton.addEventListener('click', () => {
+    const activatePlay = () => {
       playButton.remove();
       preview.remove();
       video.play();
-    });
+    };
+
+    const handleKeyUp = (event) => {
+      if (event.key === ' ' || event.key === 'Enter') {
+        const target = event.target;
+        if (target === playButton || target === video) {
+          activatePlay();
+        }
+      }
+    };
+
+    playButton.addEventListener('click', activatePlay);
+    playButton.addEventListener('keyup', handleKeyUp);
   }
 };
